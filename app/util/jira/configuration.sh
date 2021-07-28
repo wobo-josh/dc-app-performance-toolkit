ef591f7b8abdda386db80536c4ee5077e0c8372c
mmizin <mmizin@atlassian.com>
2021-07-28 15:30:24 +0300

    DCA-968:Update run_script file; Create configuration file

--- app/util/jira/configuration.sh
+++ app/util/jira/configuration.sh
@@ -23,8 +23,28 @@ if [[ ! $(systemctl status jira) ]]; then
 fi
 
 ###################    Variables section         ###################
+# Command to install psql client for Amazon Linux 2.
+# In case of different distributive, please adjust accordingly or install manually.
+INSTALL_PSQL_CMD="amazon-linux-extras install -y postgresql11"
+
+# DB config file location (dbconfig.xml)
+DB_CONFIG="/var/atlassian/application-data/jira/dbconfig.xml"
+
 # Jira version variables
 JIRA_VERSION_FILE="/media/atl/jira/shared/jira-software.version"
+
+# Depending on Jira installation directory
+JIRA_CURRENT_DIR="/opt/atlassian/jira-software/current"
+START_JIRA="${JIRA_CURRENT_DIR}/bin/start-jira.sh"
+CATALINA_PID_FILE="${JIRA_CURRENT_DIR}/work/catalina.pid"
+JIRA_SETENV_FILE="${JIRA_CURRENT_DIR}/bin/setenv.sh"
+JIRA_VERSION_FILE="/media/atl/jira/shared/jira-software.version"
+SHUT_DOWN_TOMCAT="${JIRA_CURRENT_DIR}/bin/shutdown.sh"
+
+# DB admin user name, password and DB name
+JIRA_DB_NAME="jira"
+JIRA_DB_USER="postgres"
+JIRA_DB_PASS="Password1!"
 
 # Jira/JSM supported versions
 
@@ -32,10 +52,14 @@ SUPPORTED_JIRA_VERSIONS=(8.5.15 8.13.7)
 SUPPORTED_JSM_VERSIONS=(4.5.15 4.13.7)
 
 SUPPORTED_VERSIONS=("${SUPPORTED_JIRA_VERSIONS[@]}")
+# JSM section
 if [[ ${jsm} == 1 ]]; then
+  JIRA_CURRENT_DIR="/opt/atlassian/jira-servicedesk/current"
+  JIRA_SETENV_FILE="${JIRA_CURRENT_DIR}/bin/setenv.sh"
   JIRA_VERSION_FILE="/media/atl/jira/shared/jira-servicedesk.version"
   SUPPORTED_VERSIONS=("${SUPPORTED_JSM_VERSIONS[@]}")
 fi
+
 JIRA_VERSION=$(sudo su jira -c "cat ${JIRA_VERSION_FILE}")
 if [[ -z "$JIRA_VERSION" ]]; then
   echo "ERROR: Failed to get Jira version. If your application type is JSM use flag '--jsm'." \
@@ -45,6 +69,7 @@ if [[ -z "$JIRA_VERSION" ]]; then
 fi
 echo "Jira Version: ${JIRA_VERSION}"
 
+# Datasets AWS bucket and db dump name
 DATASETS_AWS_BUCKET="https://centaurus-datasets.s3.amazonaws.com/jira"
 if [[ ${jsm} == 1 ]]; then
   DATASETS_AWS_BUCKET="https://centaurus-datasets.s3.amazonaws.com/jsm"
@@ -57,9 +82,12 @@ if [[ ${jsm} == 1 && ${small} == 1 ]]; then
   # Only JSM supports "small" dataset
   DATASETS_SIZE="small"
 fi
+DB_DUMP_NAME="db.dump"
+DB_DUMP_URL="${DATASETS_AWS_BUCKET}/${JIRA_VERSION}/${DATASETS_SIZE}/${DB_DUMP_NAME}"
 ATTACHMENTS_TAR_URL="${DATASETS_AWS_BUCKET}/${JIRA_VERSION}/${DATASETS_SIZE}/${ATTACHMENTS_TAR}"
 TMP_DIR="/tmp"
 EFS_DIR="/media/atl/jira/shared/data"
+
 ###################    End of variables section  ###################
 
 # Check if Jira version is supported
@@ -67,19 +95,29 @@ if [[ ! "${SUPPORTED_VERSIONS[*]}" =~ ${JIRA_VERSION} ]]; then
   echo "Jira Version: ${JIRA_VERSION} is not officially supported by Data Center App Performance Toolkit."
   echo "Supported Jira Versions: ${SUPPORTED_VERSIONS[*]}"
   echo "If you want to force apply an existing datasets to your Jira, use --force flag with version of dataset you want to apply:"
-  echo "e.g. ./upload_attachments --force 8.5.0"
-  echo "!!! Warning !!! This may broke your Jira instance."
+  echo "e.g. ./populate_db.sh --force 8.5.0"
+  echo "!!! Warning !!! This may break your Jira instance."
   # Check if --force flag is passed into command
   if [[ ${force} == 1 ]]; then
     # Check if passed Jira version is in list of supported
     if [[ "${SUPPORTED_VERSIONS[*]}" =~ ${version} ]]; then
+      DB_DUMP_URL="${DATASETS_AWS_BUCKET}/${version}/${DATASETS_SIZE}/${DB_DUMP_NAME}"
+      echo "Force mode. Dataset URL: ${DB_DUMP_URL}"
       ATTACHMENTS_TAR_URL="${DATASETS_AWS_BUCKET}/${version}/${DATASETS_SIZE}/${ATTACHMENTS_TAR}"
       echo "Force mode. Dataset URL: ${ATTACHMENTS_TAR_URL}"
+      # If there is no DOWNGRADE_OPT - set it
+      DOWNGRADE_OPT="Djira.downgrade.allowed=true"
+      if sudo su jira -c "! grep -q ${DOWNGRADE_OPT} $JIRA_SETENV_FILE"; then
+        sudo sed -i "s/JVM_SUPPORT_RECOMMENDED_ARGS=\"/&-${DOWNGRADE_OPT} /" "${JIRA_SETENV_FILE}"
+        echo "Flag -${DOWNGRADE_OPT} was set in ${JIRA_SETENV_FILE}"
+      fi
     else
       LAST_DATASET_VERSION=${SUPPORTED_VERSIONS[${#SUPPORTED_VERSIONS[@]}-1]}
+      DB_DUMP_URL="${DATASETS_AWS_BUCKET}/$LAST_DATASET_VERSION/${DATASETS_SIZE}/${DB_DUMP_NAME}"
       ATTACHMENTS_TAR_URL="${DATASETS_AWS_BUCKET}/$LAST_DATASET_VERSION/${DATASETS_SIZE}/${ATTACHMENTS_TAR}"
       echo "Specific dataset version was not specified after --force flag, using the last available: ${LAST_DATASET_VERSION}"
-      echo "Dataset URL: ${ATTACHMENTS_TAR_URL}"
+      echo "Database dump URL: ${DB_DUMP_URL}"
+      echo "Attachments URL: ${ATTACHMENTS_TAR_URL}"
     fi
   else
     # No force flag
@@ -88,18 +126,23 @@ if [[ ! "${SUPPORTED_VERSIONS[*]}" =~ ${JIRA_VERSION} ]]; then
 fi
 
 echo "!!! Warning !!!"
-echo    # move to a new line
-echo "This script restores attachments into Jira DC created with AWS Quickstart defaults."
+echo # move to a new line
+echo "This script restores Postgres DB from SQL DB dump for Jira DC created with AWS Quickstart defaults."
 echo "You can review or modify default variables in 'Variables section' of this script."
-echo    # move to a new line
+echo # move to a new line
 echo "Variables:"
+echo "JIRA_CURRENT_DIR=${JIRA_CURRENT_DIR}"
+echo "DB_CONFIG=${DB_CONFIG}"
+echo "JIRA_DB_NAME=${JIRA_DB_NAME}"
+echo "JIRA_DB_USER=${JIRA_DB_USER}"
+echo "JIRA_DB_PASS=${JIRA_DB_PASS}"
+echo "DB_DUMP_URL=${DB_DUMP_URL}"
 echo "EFS_DIR=${EFS_DIR}"
 echo "ATTACHMENTS_TAR_URL=${ATTACHMENTS_TAR_URL}"
-echo    # move to a new line
+echo # move to a new line
 read -p "I confirm that variables are correct and want to proceed (y/n)?  " -n 1 -r
-echo    # move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    echo "Script was canceled."
-    exit 1
+echo # move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  echo "Script was canceled."
+  exit 1
 fi
--- app/util/jira/run_scripts.sh
+++ app/util/jira/run_scripts.sh
@@ -1,11 +1,13 @@
-wget https://raw.githubusercontent.com/atlassian/dc-app-performance-toolkit/jira/dca-968-run-db-dump-and-attachments-in-parallel/app/util/jira/populate_db_variables.sh
-wget https://raw.githubusercontent.com/atlassian/dc-app-performance-toolkit/jira/dca-968-run-db-dump-and-attachments-in-parallel/app/util/jira/upload_attachments_variables.sh
+  wget https://raw.githubusercontent.com/atlassian/dc-app-performance-toolkit/jira/dca-968-run-db-dump-and-attachments-in-parallel/app/util/jira/configuration.sh
 
-source populate_db_variables.sh
-wget https://raw.githubusercontent.com/atlassian/dc-app-performance-toolkit/jira/dca-968-run-db-dump-and-attachments-in-parallel/app/util/jira/populate_db.sh && chmod +x populate_db.sh
+  sleep 2s
 
-source upload_attachments_variables.sh
-wget https://raw.githubusercontent.com/atlassian/dc-app-performance-toolkit/jira/dca-968-run-db-dump-and-attachments-in-parallel/app/util/jira/upload_attachments.sh && chmod +x populate_db.sh
+  source ${PWD}/configuration.sh
 
-./populate_db.sh 2>&1 | tee -a populate_db.log &
-./upload_attachments.sh 2>&1 | tee -a upload_attachments.log &
+
+  wget https://raw.githubusercontent.com/atlassian/dc-app-performance-toolkit/jira/dca-968-run-db-dump-and-attachments-in-parallel/app/util/jira/populate_db.sh && chmod +x populate_db.sh
+  wget https://raw.githubusercontent.com/atlassian/dc-app-performance-toolkit/jira/dca-968-run-db-dump-and-attachments-in-parallel/app/util/jira/upload_attachments.sh && chmod +x upload_attachments.sh
+
+  sleep 2s
+
+  parallel ::: "echo 'y' | ./populate_db.sh 2>&1 | tee -a populate_db.log" "echo 'y' | ./upload_attachments.sh 2>&1 | tee -a upload_attachments.log"
