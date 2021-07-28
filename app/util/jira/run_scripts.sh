0fe718679297e1620f17e152670117f85b1e79f3
mmizin <mmizin@atlassian.com>
2021-07-28 16:49:09 +0300

    DCA-968:Update run_scripts file

--- app/util/jira/run_scripts.sh
+++ app/util/jira/run_scripts.sh
@@ -2,7 +2,7 @@
 
   sleep 2s
 
-  source ${PWD}/configuration.sh
+  source echo -'y' | ${PWD}/configuration.sh
 
 
   wget https://raw.githubusercontent.com/atlassian/dc-app-performance-toolkit/jira/dca-968-run-db-dump-and-attachments-in-parallel/app/util/jira/populate_db.sh && chmod +x populate_db.sh
